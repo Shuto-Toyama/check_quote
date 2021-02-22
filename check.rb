d45d5a8454ae82cccfe694fba975e24e827739db
Stoyama <Stoyama@MacBook.local>
2021-02-22 10:44:21 +0900

    hoge5

--- check.rb
+++ check.rb
@@ -1,15 +1,14 @@
-# moviesテーブルのデータを全削除し、'db/csv_data/movie_data.csv'のデータを投入	# moviesテーブルのデータを全削除し、'db/csv_data/movie_data.csv'のデータを投入
 ImportCsv.movie_data	ImportCsv.movie_data
 
-EMAIL = 'test@example.com'	# lineテーブルのデータを全削除し、'db/csv_data/line_data.csv'のデータを投入
+EMAIL = 'test@example.com'
 PASSWORD = 'password'	ImportCsv.line_data
 
-# テストユーザーが存在しないときだけ作成	# テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: EMAIL) do |user|	User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD	  user.password = PASSWORD
-  puts 'ユーザーの初期データインポートに成功しました。'	  puts "ユーザーの初期データインポートに成功しました。"
+  puts 'ユーザーの初期データインポートに成功しました。'	 
 end
 
 p "hello"
 
 p 'hi'
+
