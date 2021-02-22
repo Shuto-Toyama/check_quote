0e2a4d2baba7e53ec0e8f26e345b657b2377ac6c
Stoyama <Stoyama@MacBook.local>
2021-02-22 10:37:22 +0900

    hoge3

--- check.rb
+++ check.rb
@@ -1,10 +1,13 @@
-"emmet.syntaxProfiles": {
-    "html": {
-        "attr_quotes": "single"
-    }
-}
+# moviesテーブルのデータを全削除し、'db/csv_data/movie_data.csv'のデータを投入	# moviesテーブルのデータを全削除し、'db/csv_data/movie_data.csv'のデータを投入
+ImportCsv.movie_data	ImportCsv.movie_data
 
-<a href=''></a>
+EMAIL = 'test@example.com'	# lineテーブルのデータを全削除し、'db/csv_data/line_data.csv'のデータを投入
+PASSWORD = 'password'	ImportCsv.line_data
 
-<div class=''>
-<a href=''>
+# テストユーザーが存在しないときだけ作成	# テストユーザーが存在しないときだけ作成
+User.find_or_create_by!(email: EMAIL) do |user|	User.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD	  user.password = PASSWORD
+  puts 'ユーザーの初期データインポートに成功しました。'	  puts "ユーザーの初期データインポートに成功しました。"
+end
+
+p "hello"
